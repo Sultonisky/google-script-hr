515245ebc10aa5f72bbe44cb4158293613817d26
sultonisky <muhsultonipml111@gmail.com>
2026-09-01 00:26:59 +0700

    fix(config): update MPR domain configuration to use HRIS settings

--- mito-hris-laravel/config/mpr.php
+++ mito-hris-laravel/config/mpr.php
@@ -1,9 +1,7 @@
 <?php
 
-$defaultHost = parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST) ?: 'localhost';
-
 return [
-    'domain' => env('MPR_DOMAIN', 'mpr.' . $defaultHost),
+    'domain' => config('hris.domains.mpr', env('MPR_DOMAIN', 'mpr.localhost')),
     'session_key' => env('MPR_SESSION_KEY', 'mpr_requestor_auth'),
     'requestor_sheet' => env('GOOGLE_SHEET_MPR_REQUESTOR', 'mpr_requestor'),
 ];
